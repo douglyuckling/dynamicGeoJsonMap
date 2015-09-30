9c29ecf28e5f551048da35fd18b07422426d1ef9
Douglas Paul <douglas.w.paul@gmail.com>
2015-09-30 17:09:33 -0400

    Topojson automation via Gulp

--- convertToTopojson.js
+++ convertToTopojson.js
@@ -1,31 +1,37 @@
-var fs = require('fs');
 var topojson = require('topojson');
 
-var inputFilePath = 'uk.geo.json';
-var outputFilePath = 'app/uk.topo.json';
+var through = require('through2');
+var gutil = require('gulp-util');
+var PluginError = gutil.PluginError;
+var convertToTopoJson = require('./convertToTopoJson.js');
 
-function convertToTopoJson(geoJson) {
-    return topojson.topology({
-        subunits: geoJson
-    }, {
-        id: function(feature) {
-            return feature.properties.SU_A3;
-        },
-        propertyTransform: function(feature) {
-            return {
-                name: feature.properties.NAME
-            };
-        },
-        quantization: null
-    });
+const GULP_PLUGIN_NAME = 'gulp-convertToTopoJson';
+
+function convertToTopojson(objects, options) {
+    return topojson.topology(objects, options);
 }
 
-fs.readFile(inputFilePath, 'utf8', function(err, data) {
-    if (err) err;
-    var geoJson = JSON.parse(data);
-    var topoJson = convertToTopoJson(geoJson);
+convertToTopojson.gulp = function(objectName, options) {
+    var stream = through.obj(function(file, enc, cb) {
+        if (file.isStream()) {
+            this.emit('error', new PluginError(GULP_PLUGIN_NAME, 'Streams are not supported!'));
+            return cb();
+        }
 
-    fs.writeFile(outputFilePath, JSON.stringify(topoJson), function(err) {
-        console.log("Saved " + outputFilePath);
+        if (file.isBuffer()) {
+            var geoJson = JSON.parse(file.contents);
+            var objects = {};
+            objects[objectName] = geoJson;
+            var topoJson = convertToTopojson(objects, options);
+            file.contents = new Buffer(JSON.stringify(topoJson));
+        }
+
+        this.push(file);
+
+        cb();
     });
-});
+
+    return stream;
+};
+
+module.exports = convertToTopojson;
--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,43 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
+var clean = require('gulp-clean');
+var rename = require("gulp-rename");
+var convertToTopoJson = require('./convertToTopoJson.js');
+
+gulp.task('clean', function() {
+    gulp.src('./dist/*')
+      .pipe(clean({force: true}));
+});
+
+gulp.task('copy-app-files', function () {
+  gulp.src('./app/**/*')
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('convert-to-topojson', function() {
+    gulp.src('./uk.geo.json')
+        .pipe(convertToTopoJson.gulp('subunits', {
+            id: function(feature) {
+                return feature.properties.SU_A3;
+            },
+            propertyTransform: function(feature) {
+                return {
+                    name: feature.properties.NAME
+                };
+            },
+            quantization: null
+        }))
+        .pipe(rename('uk.topo.json'))
+        .pipe(gulp.dest('dist/'));
+});
 
 gulp.task('connect', function () {
   connect.server({
-    root: 'app/',
+    root: 'dist/',
     port: 8008
   });
 });
+
+gulp.task('build',
+  ['copy-app-files', 'convert-to-topojson']
+);
